a32740da5959828fef941250630fe8628e51d2d2
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-16 04:20:30 -0600

    navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import '../App.css';
 import images from '../react-logo.png'
 
 export default function Navbar(){
-return (<nav>
+return (
+<nav>
     <img src={images} alt="images" width="40px" />
-    <h1>Fun facts about React</h1>
+    <h3>ReactFacts</h3>
+    <h4>React Course - Project 1</h4>
 </nav>
 );       
 }
