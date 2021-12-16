57b9a33651182227620a87b68357b5f2501243ae
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-16 04:23:19 -0600

    navabar classname

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import images from '../react-logo.png'
 export default function Navbar(){
 return (
 <nav>
-    <img src={images} alt="images" width="40px" />
-    <h3>ReactFacts</h3>
-    <h4>React Course - Project 1</h4>
+    <img src={images} alt="images" width="40px" className="nav--icon"/>
+    <h3 className="nav--logo_text">ReactFacts</h3>
+    <h4 className="nav--title">React Course - Project 1</h4>
 </nav>
 );       
 }
