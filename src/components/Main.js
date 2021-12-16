9166308c64871aca3a2577b823544d072e16338c
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-16 04:28:52 -0600

    main jsx

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,16 @@
 import '../App.css';
 
 export default function Main(){
-    return (<div>
-       <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on GitHub</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     );
 }
